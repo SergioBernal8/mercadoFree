9935f5b23fcae182b3442451c43f81fc1805fec0
Sergio Bernal <c-sbernal@nexstar.tv>
2021-05-18 11:29:57 -0500

    created base project

---
+++ mercadoFree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mercadoFree
+//
+//  Created by Sergio Bernal on 18/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
